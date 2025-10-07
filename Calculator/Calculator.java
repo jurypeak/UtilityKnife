0ce470c7649fc3db61057eae00e93229681576dd
jurypeak <jurypeak@outlook.com>
2025-10-07 08:59:30 +0000

    Added override functions

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,1 +1,30 @@
-oeesfsefs
+/**
+ * @author James MacKenzie
+ */
+
+public class Calculator implements ICalculator {
+  @Override
+  public double addition(double value1, double value2) {
+    return value1 + value2;
+  }
+
+  @Override
+  public double subtraction(double value1, double value2) {
+    return value1 - value2;
+  }
+
+  @Override
+  public double division(double value1, double value2) {
+    return value1 / value2;
+  }
+
+  @Override
+  public double multiplication(double value1, double value2) {
+    return value1 * value2;
+  }
+
+  @Override
+  public void display() {
+
+  }
+}
--- Calculator/ICalculator.java
+++ Calculator/ICalculator.java
@@ -3,13 +3,13 @@
  */
 
 public interface ICalculator {
-  public int addition(int value1, int value2);
+  public double addition(double value1, double value2);
 
-  public int subtraction(int value1, int value2);
+  public double subtraction(double value1, double value2);
 
-  public int division(int value1, int value2);
+  public double division(double value1, double value2);
 
-  public int multiplication(int value1, int value2);
+  public double multiplication(double value1, double value2);
 
   public void display();
 };
