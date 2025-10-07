0e8626f72995d31e452d7191c54110bbaebc08e1
jurypeak <jurypeak@outlook.com>
2025-10-07 08:44:03 +0000

    Added ICalculator.java

---
+++ Calculator/ICalculator.java
@@ -0,0 +1,15 @@
+/**
+ * @author James MacKenzie
+ */
+
+public interface ICalculator {
+  public int addition(int value1, int value2);
+
+  public int subtraction(int value1, int value2);
+
+  public int division(int value1, int value2);
+
+  public int multiplication(int value1, int value2);
+
+  public void display();
+};
